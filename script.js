a251850594046a48d4ca7835bbe1d92d488cc2f9
Cameron <c@cameronfrost.com>
2021-01-25 16:24:26 -0600

    Finalize project

--- script.js
+++ script.js
@@ -3,27 +3,6 @@ const entriesSection = document.querySelector("#entries");
 const entryTextbox = document.querySelector(".entry-textbox");
 const entriesNav = document.querySelector(".entries-nav");
 
-function addEntryToDom(event) {
-  event.preventDefault();
-  const entryDiv = document.createElement("div");
-  entryDiv.className = "single-entry";
-  entryDiv.innerText = entryTextbox.value;
-  entryDiv.style.display = "none";
-  entriesSection.appendChild(entryDiv);
-  entryTextbox.value = "";
-
-  const displayEntryButton = document.createElement("button");
-  displayEntryButton.className = "display-entry-button";
-  displayEntryButton.innerText = 1;
-  entriesNav.appendChild(displayEntryButton);
-}
-
-entryForm.addEventListener("submit", addEntryToDom);
-const entryForm = document.getElementById("entryForm");
-const entriesSection = document.querySelector("#entries");
-const entryTextbox = document.querySelector(".entry-textbox");
-const entriesNav = document.querySelector(".entries-nav");
-
 let count = 1;
 function addEntryToDom(event) {
   event.preventDefault();
